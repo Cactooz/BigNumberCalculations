b545ce41a84f553949d8fd6988753253ba4437a4
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-29 10:55:19 +0100

    Get string lengths

--- src/com/company/Calculations.java
+++ src/com/company/Calculations.java
@@ -2,7 +2,12 @@ package com.company;
 
 public class Calculations {
 	public static String addition(String num1, String num2) {
-	
+		
+		//Get the length of the strings
+		int length1 = num1.length();
+		int length2 = num2.length();
+		
+		
 	}
 	
 	public static String subtraction(String num1, String num2) {
