067679851ea952e222936cdd490548a61c397c70
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-29 10:50:25 +0100

    Static methods for addition and subtraction

--- src/com/company/Calculations.java
+++ src/com/company/Calculations.java
@@ -1,5 +1,11 @@
 package com.company;
 
 public class Calculations {
-
+	public static String addition(String num1, String num2) {
+	
+	}
+	
+	public static String subtraction(String num1, String num2) {
+	
+	}
 }
