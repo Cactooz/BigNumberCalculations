2f834cccde0f995c3250e6b06f37e1e8bcc9f286
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-29 15:41:29 +0100

    Get the length of the strings

--- src/com/company/Calculations.java
+++ src/com/company/Calculations.java
@@ -59,6 +59,9 @@ public class Calculations {
 	}
 	
 	public static String subtraction(String num1, String num2) {
-	
+		
+		//Get the length of the strings
+		int length1 = num1.length();
+		int length2 = num2.length();
 	}
 }
